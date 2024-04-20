a1ebd39eaaccb224d2512e9db30fc7ab8f941100
Nima Karimi <nimakarimi676@gmail.com>
2024-04-20 17:55:36 +0200

    ref: commented the md editor

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import appwriteService from '../appwrite/config.js';
 
 function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className='w-full bg-gray-100 rounded-xl p-4'>
-        <div className='w-full justify-center mb-4'>
+      <div className='w-full p-4 bg-gray-100 rounded-xl'>
+        <div className='justify-center w-full mb-4'>
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
